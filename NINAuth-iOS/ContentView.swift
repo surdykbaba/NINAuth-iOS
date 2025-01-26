a22a3524be4ae7a1d1114e01036a0e71e5287a96
Liimmaa <halimammegwa@gmail.com>
2025-01-26 21:53:12 +0100

    Initial Commit

---
+++ NINAuth-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NINAuth-iOS
+//
+//  Created by Chioma Amanda Mmegwa  on 26/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NINAuth-iOS/NINAuth_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NINAuth_iOSApp.swift
+//  NINAuth-iOS
+//
+//  Created by Chioma Amanda Mmegwa  on 26/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct NINAuth_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
