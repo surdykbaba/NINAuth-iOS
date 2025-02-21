4d9ae9e34143e754a7099cb0f2dcd5577d4de010
Maxwell <maxsteelobinna@gmail.com>
2025-02-21 13:18:03 +0100

    WIP login With NIN

--- NINAuth-iOS/Network/ApiService/AuthService.swift
+++ NINAuth-iOS/Network/ApiService/AuthService.swift
@@ -102,48 +102,27 @@ struct AuthService: AuthProtocol {
     }
     
     @MainActor
-    func regenerateCode(sessionID: String) async -> Result<JSON, ErrorBag> {
-        do {
-            var request = NetworkResponseModel.generateSecretHeader(endpoint: URLs.RE_GENERATE_CODE, authoriseHeader: true)
-            request.httpMethod = APIVerb.GET.rawValue
-            let (data, response) = try await URLSession.shared.data(for: request)
-            let httpResponse = response as? HTTPURLResponse
-            let networkResponse = NetworkResponseModel(statusCode: (httpResponse?.statusCode ?? 0))
-            if(networkResponse.isSuccess()) {
+    func loginWithNIN(loginWithNIN: LoginWithNIN) async -> Result<User, ErrorBag> {
+        let networkResponse = await Service.init().post(URLs.LOGIN_NIN, params: loginWithNIN, authoriseHeader: false)
+        switch networkResponse.isSuccess() {
+        case true:
+            do {
                 let realm = try await Realm()
+                var user = User()
                 try await realm.asyncWrite {
-                    let token = realm.objects(Token.self).first
-                    token?.requestCode = networkResponse.getJson()?["data"]["qrCode"].stringValue ?? ""
+                    let token = Token(value: networkResponse.getJson()?["tokens"])
+                    user = token.user ?? User()
+                    realm.deleteAll()
+                    realm.add(token)
+                    realm.add(user)
+                    print(user)
                 }
-                return .success(true)
-            }else {
-                let networkResponseFailed = NetworkResponseModel(statusCode: networkResponse.getStatusCode(), data: data)
-                return .failure(networkResponseFailed.getErrorBag())
+                return .success(user.freeze())
+            }catch {
+                Log.error(error.localizedDescription)
+                return .failure(ErrorBag())
             }
-        }catch {
-            let networkResponse = NetworkResponseModel(statusCode: 0, data: nil, errorMessage: error.localizedDescription)
-            return .failure(networkResponse.getErrorBag())
-        }
-    }
-    
-    func startSession(startSessionRequest: StartSessionRequest) async -> Result<SessionResponse, ErrorBag> {
-        do {
-            var request = NetworkResponseModel.generateSecretHeader(endpoint: URLs.START_SESSION, authoriseHeader: true)
-            request.httpMethod = APIVerb.POST.rawValue
-            let jsonData = try? JSONEncoder().encode(startSessionRequest)
-            request.httpBody = jsonData
-            let (data, response) = try await URLSession.shared.data(for: request)
-            let httpResponse = response as? HTTPURLResponse
-            let networkResponse = NetworkResponseModel(statusCode: (httpResponse?.statusCode ?? 0))
-            if(networkResponse.isSuccess()) {
-                let res = try JSONDecoder().decode(SessionResponse.self, from: networkResponse.getJson()!["data"].data())
-                return .success(res)
-            }else {
-                let networkResponseFailed = NetworkResponseModel(statusCode: networkResponse.getStatusCode(), data: data)
-                return .failure(networkResponseFailed.getErrorBag())
-            }
-        }catch {
-            let networkResponse = NetworkResponseModel(statusCode: 0, data: nil, errorMessage: error.localizedDescription)
+        default:
             return .failure(networkResponse.getErrorBag())
         }
     }
