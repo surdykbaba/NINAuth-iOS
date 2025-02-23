057d60a8f1b09d91e738808fb71f87539aa0d879
Maxwell <maxsteelobinna@gmail.com>
2025-02-23 19:29:25 +0100

    added bluSalt

---
+++ BlusaltLivenessOnly.xcframework/ios-arm64_x86_64-simulator/BlusaltLivenessOnly.framework/Headers/LivenessOnlyFramework.h
@@ -0,0 +1,20 @@
+//
+//  LivenessOnlyFramework.h
+//  LivenessOnlyFramework
+//
+//  Created by Folarin Opeyemi on 05/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+//! Project version number for LivenessOnlyFramework.
+FOUNDATION_EXPORT double LivenessOnlyFrameworkVersionNumber;
+
+//! Project version string for LivenessOnlyFramework.
+FOUNDATION_EXPORT const unsigned char LivenessOnlyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LivenessOnlyFramework/PublicHeader.h>
+
+
