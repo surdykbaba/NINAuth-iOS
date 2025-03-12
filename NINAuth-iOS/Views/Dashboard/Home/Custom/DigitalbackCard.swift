d926c836fd3878b5be54e840bdda2d7cb7c2f65d
Arogundade Abdulqoyum <88770005+Kostume13@users.noreply.github.com>
2025-03-12 15:45:25 +0100

    Done with ios card

--- NINAuth-iOS/Views/Dashboard/Home/Custom/DigitalbackCard.swift
+++ NINAuth-iOS/Views/Dashboard/Home/Custom/DigitalbackCard.swift
@@ -1,9 +1,3 @@
-//  DigitalbackCard.swift
-//  NINAuth-iOS
-//
-//  Created by Arogundade Qoyum on 12/03/2025.
-//
-
 import SwiftUI
 import RealmSwift
 
@@ -11,25 +5,34 @@ struct DigitalbackCard: View {
     @ObservedResults(User.self) var user
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-                
-                Text("2045 4664 7474 7474".localized)
-                    .customFont(.title, fontSize: 24)
-                    .foregroundColor(.gray)
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .offset(x: -25)
-                    .padding(.top, 180)
-            
-            
-            
-            Spacer()
-        }
-        .frame(height: 242)
-        .frame(maxWidth: .infinity, maxHeight: 242, alignment: .leading)
-        .background(
+        ZStack {
             Image("NinBack")
                 .resizable()
-        )
+                .scaledToFill()
+                .frame(height: 242)
+                .clipped()
+
+            VStack {
+                Spacer() // Pushes the first text to the center
+                
+                Text("cvv234") // First text in the center
+                    .customFont(.body, fontSize: 16)
+                    .foregroundColor(.gray)
+                    .offset(x: -120)
+                    .padding(.top, 30)
+
+                Spacer() // Creates space between both texts
+                
+                Text("2045 4664 7474 7474".localized) // Second text at the bottom
+                    .customFont(.title, fontSize: 24)
+                    .foregroundColor(.gray)
+                    .offset(x: -25)
+                    .padding(.bottom, 20) // Adjust as needed
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+        .frame(height: 242)
+        .frame(maxWidth: .infinity)
     }
 }
 
