7e152650840aff225d26c0f9f1e3f5578ce30358
Arogundade Abdulqoyum <88770005+Kostume13@users.noreply.github.com>
2025-04-15 14:56:42 +0100

    Added W3 standard credentials to Qrcode

--- NINAuth-iOS/Views/Dashboard/Home/Custom/DigitalbackCard.swift
+++ NINAuth-iOS/Views/Dashboard/Home/Custom/DigitalbackCard.swift
@@ -3,61 +3,85 @@
 //  NINAuth-iOS
 //
 //  Created by Arogundade Qoyum on 15/04/2025.
-
 import SwiftUI
 import RealmSwift
 import CoreImage.CIFilterBuiltins
 
 struct DigitalbackCard: View {
     @ObservedResults(User.self) var user
-
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
 
+    var qrImage: UIImage {
+        guard let currentUser = user.first else {
+            return UIImage(systemName: "xmark.circle")!
+        }
+
+        let credentialSubject: [String: String] = [
+            "id": "did:nin:\(currentUser.nin)",
+            "firstName": currentUser.first_name ?? "",
+            "middleName": currentUser.middle_name ?? "",
+            "lastName": currentUser.last_name ?? "",
+            "gender": currentUser.gender ?? "",
+            "stateOfOrigin": currentUser.origin_state ?? "",
+            "lga": currentUser.origin_local_government ?? "",
+            "dateOfBirth": currentUser.getDOB()
+        ]
+
+        let payload: [String: Any] = [
+            "@context": ["https://www.w3.org/2018/credentials/v1"],
+            "type": ["VerifiableCredential"],
+            "credentialSubject": credentialSubject,
+            "issuer": "https://nin.gov.ng",
+            "issuanceDate": Date().iso8601String()
+        ]
+
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: payload),
+              let qrFilter = CIFilter(name: "CIQRCodeGenerator") else {
+            return UIImage(systemName: "xmark.circle")!
+        }
+
+        qrFilter.setValue(jsonData, forKey: "inputMessage")
+        qrFilter.setValue("H", forKey: "inputCorrectionLevel")
+
+        guard let outputImage = qrFilter.outputImage else {
+            return UIImage(systemName: "xmark.circle")!
+        }
+
+        let scaledImage = outputImage.transformed(by: CGAffineTransform(scaleX: 7, y: 7))
+
+        if let cgimg = context.createCGImage(scaledImage, from: scaledImage.extent) {
+            return UIImage(cgImage: cgimg)
+        }
+
+        return UIImage(systemName: "xmark.circle")!
+    }
+
     var body: some View {
-        ZStack(alignment: .topLeading) {
+        ZStack(alignment: .bottomTrailing) {
             Image("NinID_Back_new")
                 .resizable()
                 .frame(height: 242)
                 .clipped()
 
-            if let currentUser = user.first {
-                let userData: [String: String] = [
-                    "surname": currentUser.last_name ?? "",
-                    "first_name": currentUser.first_name ?? "",
-                    "middle_name": currentUser.middle_name ?? "",
-                    "gender": currentUser.gender ?? "",
-                    "nin": currentUser.nin ?? "",
-                    "dob": currentUser.getDOB(),
-                    "origin_state": currentUser.origin_state ?? "N/A",
-                    "origin_lga": currentUser.origin_local_government ?? "N/A"
-                ]
-
-                if let jsonData = try? JSONEncoder().encode(userData),
-                   let jsonString = String(data: jsonData, encoding: .utf8),
-                   let qrImage = generateQRCode(from: jsonString) {
-                    Image(uiImage: qrImage)
-                        .interpolation(.none)
-                        .resizable()
-                        .frame(width: 64, height: 64)
-                        .padding(.leading, 275)
-                        .padding(.top, 155)
-                }
-            }
+            Image(uiImage: qrImage)
+                .interpolation(.none)
+                .resizable()
+                .frame(width: 100, height: 100)
+               
+                .cornerRadius(4)
+                
+                .offset(x: -10)
+                .offset(y: -10)
         }
-        .frame(maxWidth: 370, maxHeight: 242, alignment: .leading)
+        .frame(maxWidth: 370, maxHeight: 242)
     }
+}
 
-    func generateQRCode(from string: String) -> UIImage? {
-        filter.message = Data(string.utf8)
-        if let outputImage = filter.outputImage {
-            let transform = CGAffineTransform(scaleX: 6, y: 6)
-            let scaledImage = outputImage.transformed(by: transform)
-            if let cgimg = context.createCGImage(scaledImage, from: scaledImage.extent) {
-                return UIImage(cgImage: cgimg)
-            }
-        }
-        return nil
+extension Date {
+    func iso8601String() -> String {
+        let formatter = ISO8601DateFormatter()
+        return formatter.string(from: self)
     }
 }
 
